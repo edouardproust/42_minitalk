ff0e8a6d4da1bd8042af0a01b82eb1023ecdba8d
Edouard Proust <contact@edouardproust.dev>
2025-01-06 18:13:38 +0100

    update

--- src/client.c
+++ src/client.c
@@ -6,72 +6,51 @@
 /*   By: eproust <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/03 11:49:34 by eproust           #+#    #+#             */
-/*   Updated: 2025/01/03 16:53:31 by eproust          ###   ########.fr       */
+/*   Updated: 2025/01/06 17:29:21 by eproust          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-static	int	set_signals(sigset_t *sigset, char *msg)
+static	int	send_message(int server_pid, char *msg)
 {
-	int	msglen;
-	int	siglen;
-	int	i;
-	int j;
+	size_t len;
+	size_t	i;
+	int		j;
+	int		signal;
 
-	msglen = ft_strlen(msg);
-	siglen = 0;
-	i = 0;
-	while (i < msglen)
+	len = ft_strlen(msg);
+	i = -1;
+	while (++i <= len)
 	{
-		j = 0;
-		while (j < 8)
+		j = -1;
+		while (++j < 8)
 		{
-			sigset[siglen] = (c >> (7 - i)) & 1;
-			j++;
-			siglen++;
+			signal = (msg[i] >> (7 - j)) & 1;
+			ft_printf("%d", signal);
+			if (signal == 0)
+				signal = SIGUSR1;
+			else
+				signal = SIGUSR2;	
+			if (kill(server_pid, signal) == -1)
+				return (0);
+			usleep(100);
 		}
-		i++;
-	}
-	return (siglen);
-}
-
-static int	send_sigset(int pid, sigset_t sigset, int siglen)
-{
-	int	i;
-	int	signal;
-
-	i = 0;
-	while (i < siglen)
-	{
-		signal = SIGUSR1;
-		if (sigset[i] == 1)
-			signal = SIGUSR2;
-		if (kill(pid, SIGUSR2) == -1)
-			return (0);
-		i++;
 	}
 	return (1);
 }
 
 int	main(int ac, char **av)
 {
-	int			pid;
-	int			msg_sent;
-	sigset_t	sigset;
-	int			siglen;
+	int			server_pid;
 
 	if (ac != 3)
 		return (ft_printf("Usage: ./client <server PID> <message>\n"), 1);
-	pid = ft_atoi(av[1]);
-	if (!pid)
+	server_pid = ft_atoi(av[1]);
+	if (!server_pid)
 		return (ft_printf("Error\n"), 1);
 	// TODO: More checks on args here?
-	sigemptyset(&sigset);
-	siglen = fill_sigset(&sigset, av[2]);
-	if (siglen == 0)	
-		return (ft_printf("Error\n"), 1);
-	if (!send_sigset(pid, sigset, siglen))
+	if (!send_message(server_pid, av[2]))
 		return (ft_printf("Error: Failed to send message\n"), 1);
 	return (0);
 }
--- src/server.c
+++ src/server.c
@@ -6,19 +6,49 @@
 /*   By: eproust <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/03 11:49:34 by eproust           #+#    #+#             */
-/*   Updated: 2025/01/03 15:18:39 by eproust          ###   ########.fr       */
+/*   Updated: 2025/01/06 18:12:18 by eproust          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
+char	*g_signals = NULL;
+
+void	signal_handler(int signal)
+{
+	char	append[2];
+
+	if (signal == SIGUSR1)
+		append[0] = 0 + '0';
+	else
+		append[0] = 1 + '0';
+	append[1] = 0;
+	g_signals = ft_strjoin(g_signals, append);
+	if (!g_signals)
+	{
+		ft_printf("Error"); // TODO
+		return ;
+	}
+	//ft_printf("%s\n", g_signals);
+}
+
 int	main(void)
 {
+	size_t	len;
+
 	ft_printf("Server PID: %d\n", getpid());
+	struct sigaction sa;
+	sa.sa_handler = signal_handler;
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = 0;
+	if (sigaction(SIGUSR1, &sa, NULL) == -1 || sigaction(SIGUSR2, &sa, NULL) == -1)
+		return (ft_printf("Error"), 1); // TODO
 	while (1)
 	{
-		// Receive signals
-		// Decrypt signals (SIGUSR1 or SIGUSR2)
+		pause();
+		len = ft_strlen(g_signals);
+		if (len > 7 && ft_strncmp(g_signals + (len - 8), "00000000", 8) == 0)
+			ft_printf("%s\n", g_signals);
 	}
 	return (0);
 }
